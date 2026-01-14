10e6d8821f7c64712ea20def8c7a254daad275c2
carlosninacio <cigramirez@poligran.edu.co>
2026-01-13 19:35:44 -0500

    plantilla proyecto springboot

---
+++ nitromedicas-back/src/main/java/cr/nitromedicas/NitromedicasApplication.java
@@ -0,0 +1,13 @@
+package cr.nitromedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitromedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitromedicasApplication.class, args);
+	}
+
+}
---
+++ nitromedicas-back/src/test/java/cr/nitromedicas/NitromedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.nitromedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitromedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
