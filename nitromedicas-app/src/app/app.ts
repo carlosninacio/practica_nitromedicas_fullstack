34e652688f7414dec88267ff5a0c2205e17b9275
carlosninacio <cigramirez@poligran.edu.co>
2026-01-13 20:47:57 -0500

    Configuración basica Angular e instalación Bootstrap

--- nitromedicas-app/src/app/app.ts
+++ nitromedicas-app/src/app/app.ts
@@ -1,11 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('nitromedicas-app');
