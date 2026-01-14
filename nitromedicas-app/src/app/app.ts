78606442a323094b5cc4c06571171537a1817369
carlosninacio <cigramirez@poligran.edu.co>
2026-01-14 14:24:26 -0500

    creación componente producto-lista

--- nitromedicas-app/src/app/app.ts
+++ nitromedicas-app/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { ProductoLista } from "./producto-lista/producto-lista";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html'
+  templateUrl: './app.html',
+  imports: [ProductoLista]
 })
 export class App {
   protected readonly title = signal('nitromedicas-app');
