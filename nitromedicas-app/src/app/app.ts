e45c99f3a4e00f25226468c15cb40618148bf8df
carlosninacio <cigramirez@poligran.edu.co>
2026-01-14 15:12:50 -0500

    Manejo de rutas para el inicio

--- nitromedicas-app/src/app/app.ts
+++ nitromedicas-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ProductoLista } from "./producto-lista/producto-lista";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [ProductoLista]
+  imports: [RouterLink, RouterLinkActive, RouterOutlet]
 })
 export class App {
   protected readonly title = signal('nitromedicas-app');
