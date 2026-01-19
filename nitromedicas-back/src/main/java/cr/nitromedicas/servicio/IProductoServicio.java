1347c2ebee817af7f997e0420ab4c3b9d80b0438
carlosninacio <cigramirez@poligran.edu.co>
2026-01-19 12:04:13 -0500

    Petición POST para agregar producto al inventario

--- nitromedicas-back/src/main/java/cr/nitromedicas/servicio/IProductoServicio.java
+++ nitromedicas-back/src/main/java/cr/nitromedicas/servicio/IProductoServicio.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface IProductoServicio {
     List<Producto> listarProductos();
     Producto buscarProductoPorId(Integer idProducto);
-    void guardarProducto(Producto producto);
+    Producto guardarProducto(Producto producto);
     void eliminarProductoPorId(Integer idProducto);
     void agregarExistencia(Integer idProducto, int cantidad);
     void quitarExistencia(Integer idProducto, int cantidad);
