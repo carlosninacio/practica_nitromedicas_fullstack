6297f327cc3bf1e001fe14af5ae372e305414392
carlosninacio <cigramirez@poligran.edu.co>
2026-01-20 18:23:40 -0500

    Botones de agregar y quitar stock sin funcionalidad y componentes

---
+++ nitromedicas-app/src/app/agregar-stock/agregar-stock.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-stock',
+  imports: [],
+  templateUrl: './agregar-stock.html'
+})
+export class AgregarStock {
+
+}
